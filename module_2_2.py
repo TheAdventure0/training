fc9429cfe4d8e634a4c1c46dec3367ff6a2b6661
TheAdventure0 <sterich000@gmail.com>
2024-11-19 00:34:02 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,18 @@
+first = 35
+input(first)
+second = 46
+input(second)
+third = 35
+input(third)
+
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
+
+
+
+
+
