110358f7adb1a9efdb9ff2d9e8c00dca74b83797
TheAdventure0 <sterich000@gmail.com>
2024-11-19 21:49:51 +0300

    module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -1,6 +1,9 @@
-first = input(35)
-second = input(46)
-third = input(35)
+first = 35
+input(35)
+second = 46
+input(46)
+third = 35
+input(35)
 
 if first == second == third:
     print(3)
@@ -8,8 +11,3 @@ elif first == second or first == third or second == third:
     print(2)
 else:
     print(0)
-
-
-
-
-
