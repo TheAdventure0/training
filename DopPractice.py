2bb5582be1ba4fa848dc41849265b16e0b72bfe0
TheAdventure0 <sterich000@gmail.com>
2025-01-07 00:20:36 +0300

    DopPractice.py

--- DopPractice.py
+++ DopPractice.py
@@ -1,30 +1,13 @@
-def password(n):
-    pairs = []
-    used = set()
+for n in range(3, 21):
+    result = ''
+    used_pairs = set()
 
-    for i in range(1, n + 1):
-        if i in used:
-            continue
+    for i in range(1, 21):
+        for a in range(i + 1, 21):
+            pair_sum = i + a
+            if n % pair_sum == 0 and (i, a) not in used_pairs and (a, i) not in used_pairs:
+                result += str(i) + str(a)
+                used_pairs.add((i, a))
+                used_pairs.add((a, i))
 
-        for a in range(i, n + 1):
-            if a in used:
-                continue
-
-            if (i + a) != 0 and n % (i + a) == 0:
-                pairs.append((i, a))
-                used.add(i)
-                used.add(a)
-                break
-
-    passw_ = ""
-    for i, a in pairs:
-        passw_ += str(i) + str(a)
-
-    return passw_
-
-# print(password(3))
-# print(password(4))
-# print(password(5))
-# print(password(6))
-# print(password(7))
-
+    print(f'Число: {n}, Пароль: {result}')
